ba189f0a6d3ec35b03057d2094c999f978f9982d
Apzmie <aserd12@gmail.com>
2026-05-15 02:02:07 +0900

    Update RunModel.cs

--- unity_environments/RunModel.cs
+++ unity_environments/RunModel.cs
@@ -5,22 +5,21 @@ using System.Threading.Tasks;
 public class RunModel : MonoBehaviour
 {
     public ModelAsset modelAsset;
-    public Transform ball;
-    public Transform car;
+    public Transform target;
     
-    private float stiffness = 20f;
-    private float damping = 10f;
-    private float forceLimit = 40f;
-    private ArticulationBody aBody;
-    private ArticulationBody[] joints;
-    private float[] startJointPositions;
-    private float[] lowerLimits;
-    private float[] upperLimits;
+    private float spring = 0f;
+    private float damper = 100f;
+    private float maxForce = 10000f;
+    private float maxVelocity = 10f;
+    private float targetRange = 8f;
+    
+    private Rigidbody rBody;
+    private ConfigurableJoint[] joints;
+    private Rigidbody[] jointRbs;
+    private Vector3[] jointStartPos;
+    private Quaternion[] jointStartRot;
     private Vector3 startPos;
     private Quaternion startRot;
-    private float Tanh(float x) => (float)System.Math.Tanh(x);
-    private Vector3 lastPosition;
-    private float[] lastActions;
 
     private Model runtimeModel;
     private Worker worker;
@@ -30,46 +29,38 @@ public class RunModel : MonoBehaviour
     
     private int decisionCounter = 0;
     private int decisionPeriod = 5;
-    
-    private Vector3 carStartPos;
-    private Quaternion carStartRot;
 
     private void Start()
     {
-        aBody = GetComponent<ArticulationBody>();
-        joints = GetComponentsInChildren<ArticulationBody>();
-        joints = System.Array.FindAll(joints, j => j != aBody);
-        startJointPositions = new float[joints.Length];
-        lowerLimits = new float[joints.Length];
-        upperLimits = new float[joints.Length];
+        rBody = GetComponent<Rigidbody>();
+        joints = GetComponentsInChildren<ConfigurableJoint>();
+        jointRbs = new Rigidbody[joints.Length];
+        
+        jointStartPos = new Vector3[joints.Length];
+        jointStartRot = new Quaternion[joints.Length];
+
         for (int i = 0; i < joints.Length; i++)
         {
-            var joint = joints[i];
-            var drive = joint.xDrive;
-
-            startJointPositions[i] = joints[i].jointPosition[0] * Mathf.Rad2Deg;
-
-            lowerLimits[i] = drive.lowerLimit;
-            upperLimits[i] = drive.upperLimit;
-
-            drive.stiffness = stiffness;
-            drive.damping = damping;
-            drive.forceLimit = forceLimit;
-            drive.targetVelocity = 0f;
-            joint.xDrive = drive;
+            jointRbs[i] = joints[i].GetComponent<Rigidbody>();
+            jointStartPos[i] = joints[i].transform.localPosition;
+            jointStartRot[i] = joints[i].transform.localRotation;
+            
+            ConfigurableJoint joint = joints[i];
+            JointDrive drive = joint.angularXDrive;
+            drive.positionSpring = spring;
+            drive.positionDamper = damper;
+            drive.maximumForce = maxForce;
+            joint.angularXDrive = drive;
         }
-
-        startPos = transform.position;
-        startRot = transform.rotation;
-        lastActions = new float[joints.Length];
+        
+        startPos = transform.localPosition;
+        startRot = transform.localRotation; 
 
         runtimeModel = ModelLoader.Load(modelAsset);
         worker = new Worker(runtimeModel, BackendType.CPU);
         
         ResetAgent();
-        
-        carStartPos = car.position;
-        carStartRot = car.rotation;
+        ResetTargetPosition();
     }
 
     private void FixedUpdate()
@@ -84,39 +75,17 @@ public class RunModel : MonoBehaviour
             decisionCounter--;
         }
         
-        float agentDistance = Vector3.Distance(transform.localPosition, ball.localPosition);
-        float carDistance = Vector3.Distance(car.localPosition, ball.localPosition);              
-
-        if (agentDistance < 1.0f)
+        float disToTarget = Vector3.Distance(transform.localPosition, target.localPosition);           
+        if (disToTarget < 0.75f)
         {
             ScoreManager.instance.AddAIScore();
-            ResetAgent();
-            ResetCar();
-        }
-        
-        if (carDistance < 1.0f)
-        {
-            ScoreManager.instance.AddPlayerScore();
-            ResetAgent();
-            ResetCar();
+            ResetTargetPosition();
         }
         
         if (transform.localPosition.y < -1.0f)
         {
             ResetAgent();
-        }
-        
-        if (car.localPosition.y < -1.0f)
-        {
-            ResetCar();
-        }
-    }
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Ground"))
-        {
-            ResetAgent();
+            ScoreManager.instance.MinusAIScore();
         }
     }
 
@@ -124,53 +93,30 @@ public class RunModel : MonoBehaviour
     {
         inferencing = true;
         
-        float[] observation = new float[38];
+        float[] observation = new float[11];
         
-        int index = 0;      
-        Vector3 localVel = transform.InverseTransformDirection(aBody.linearVelocity);
-        observation[index++] = Tanh(localVel.x / 2f);
-        observation[index++] = Tanh(localVel.y / 2f);
-        observation[index++] = Tanh(localVel.z / 2f);
+        Vector3 linVel = TanhVector(transform.InverseTransformDirection(rBody.linearVelocity) / 10f);
+        observation[0] = linVel.x;
+        observation[1] = linVel.y;
+        observation[2] = linVel.z;
         
-        Vector3 localAngVel = transform.InverseTransformDirection(aBody.angularVelocity);
-        observation[index++] = Tanh(localAngVel.x / 2f);
-        observation[index++] = Tanh(localAngVel.y / 2f);
-        observation[index++] = Tanh(localAngVel.z / 2f);
+        Vector3 angVel = TanhVector(transform.InverseTransformDirection(rBody.angularVelocity) / 10f);
+        observation[3] = angVel.x;
+        observation[4] = angVel.y;
+        observation[5] = angVel.z;
         
-        Vector3 upVector = transform.InverseTransformDirection(Vector3.up);
-        observation[index++] = upVector.x;
-        observation[index++] = upVector.y;
-        observation[index++] = upVector.z;
-
         for (int i = 0; i < joints.Length; i++)
         {
-            float currentPos = joints[i].jointPosition[0] * Mathf.Rad2Deg;
-            float normalizedPos = Mathf.InverseLerp(lowerLimits[i], upperLimits[i], currentPos) * 2f - 1f;
-            observation[index++] = normalizedPos;
-            
-            float currentVel = joints[i].jointVelocity[0];
-            observation[index++] = Tanh(currentVel / 2f);
+            Vector3 localAngVel = joints[i].transform.InverseTransformDirection(jointRbs[i].angularVelocity);
+            observation[6 + i] = (float)System.Math.Tanh(localAngVel.x / 10f);
         }
         
-        for (int i = 0; i < lastActions.Length; i++)
-        {
-            observation[index++] = lastActions[i];
-        }
-        
-        Vector3 toBall = ball.position - transform.position;
+        Vector3 localTargetPos = TanhVector(transform.InverseTransformPoint(target.position) / 10f);
+        observation[8] = localTargetPos.x;
+        observation[9] = localTargetPos.y;
+        observation[10] = localTargetPos.z;
 
-        Vector3 dirToBall = toBall.normalized;
-        observation[index++] = dirToBall.x;
-        observation[index++] = dirToBall.y;
-        observation[index++] = dirToBall.z;
-        
-        float distToBall = toBall.magnitude;
-        observation[index++] = distToBall / 40f;
-        
-        float laneOffset = transform.localPosition.x - (-23.51f);
-        observation[index++] = laneOffset / 2.5f;
-
-        TensorShape shape = new TensorShape(1, 38);
+        TensorShape shape = new TensorShape(1, 11);
         inputTensor?.Dispose();
         inputTensor = new Tensor<float>(shape, observation);
         worker.Schedule(inputTensor);
@@ -181,17 +127,18 @@ public class RunModel : MonoBehaviour
         
         for (int i = 0; i < joints.Length; i++)
         {
-            float action = cpuCopyTensor[i];
-            lastActions[i] = action;
-            float targetAngle = Mathf.Lerp(lowerLimits[i], upperLimits[i], (action + 1f) / 2f);
-            
-            var drive = joints[i].xDrive;
-            drive.target = targetAngle;
-            joints[i].xDrive = drive;
+            float actionValue = cpuCopyTensor[i];
+            float targetVel = actionValue * maxVelocity;
+            joints[i].targetAngularVelocity = new Vector3(-targetVel, 0, 0);
         }
         
         cpuCopyTensor?.Dispose();
         inferencing = false;
+    }
+
+    private Vector3 TanhVector(Vector3 v)
+    {
+        return new Vector3((float)System.Math.Tanh(v.x), (float)System.Math.Tanh(v.y), (float)System.Math.Tanh(v.z));
     }
 
     private void OnDestroy()
@@ -200,36 +147,33 @@ public class RunModel : MonoBehaviour
         worker?.Dispose();
     }
     
-    public void ResetAgent()
+    private void ResetTargetPosition()
     {
-        aBody.TeleportRoot(startPos, startRot);
-        aBody.linearVelocity = Vector3.zero;
-        aBody.angularVelocity = Vector3.zero;
-
-        for (int i = 0; i < joints.Length; i++)
-        {
-            joints[i].jointPosition = new ArticulationReducedSpace(startJointPositions[i] * Mathf.Deg2Rad);
-            joints[i].jointVelocity = new ArticulationReducedSpace(0f);
-
-            var drive = joints[i].xDrive;
-            drive.target = startJointPositions[i];
-            drive.targetVelocity = 0f;
-            joints[i].xDrive = drive;
-        }
-
-        Physics.SyncTransforms();
-        lastPosition = transform.localPosition;
-        System.Array.Clear(lastActions, 0, lastActions.Length);
-        decisionCounter = 0;
+        Vector3 randomPos = new Vector3(
+            UnityEngine.Random.Range(-targetRange, targetRange),
+            target.localPosition.y,
+            UnityEngine.Random.Range(-targetRange, targetRange)
+        );        
+        target.localPosition = randomPos;
     }
     
-    private void ResetCar()
+    public void ResetAgent()
     {
-        car.position = carStartPos;
-        car.rotation = carStartRot;
-
-        Rigidbody rb = car.GetComponent<Rigidbody>();
-        rb.linearVelocity = Vector3.zero;
-        rb.angularVelocity = Vector3.zero;
+        transform.localPosition = startPos;
+        transform.localRotation = startRot;
+        rBody.linearVelocity = Vector3.zero;
+        rBody.angularVelocity = Vector3.zero;
+        
+        for (int i = 0; i < joints.Length; i++)
+        {
+            joints[i].transform.localPosition = jointStartPos[i];
+            joints[i].transform.localRotation = jointStartRot[i];
+            
+            jointRbs[i].linearVelocity = Vector3.zero;
+            jointRbs[i].angularVelocity = Vector3.zero;
+            joints[i].targetAngularVelocity = Vector3.zero;
+        }        
+        Physics.SyncTransforms();
+        decisionCounter = 0;
     }
 }
